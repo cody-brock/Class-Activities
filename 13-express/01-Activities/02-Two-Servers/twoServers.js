b6be7c7311d1ec42345ffe04b2c0b0b14a223810
eternal golden bread <codylewisbrock@gmail.com>
2019-12-21 10:51:31 -0800

    adds work to servers.  created my first server! And another file with two servers running simultaneously

---
+++ 13-express/01-Activities/02-Two-Servers/twoServers.js
@@ -0,0 +1,21 @@
+const http = require('http');
+
+const PORT1 = 7000;
+const PORT2 = 7050;
+
+function handleRequest1(request, response) {
+  response.end("I am a beautiful butterly");
+}
+function handleRequest2(request, response) {
+  response.end("Box of chocolates");
+}
+
+const server1 = http.createServer(handleRequest1);
+const server2 = http.createServer(handleRequest2);
+
+server1.listen(PORT1, function() {
+  console.log("Server listening on: http://localhost:" + PORT1);
+});
+server2.listen(PORT2, function() {
+  console.log("Server listening on: http://localhost:" + PORT2);
+});
